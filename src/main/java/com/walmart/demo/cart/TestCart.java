1dc2cf4a10ba0d2abbced38b9f068879fcbd517b
vikas8352 <vikas.8352@gmail.com>
2016-11-07 11:53:01 +0530

    first

--- src/main/java/com/walmart/demo/cart/TestCart.java
+++ src/main/java/com/walmart/demo/cart/TestCart.java
@@ -5,7 +5,7 @@ package com.walmart.demo.cart;
  */
 public class TestCart {
     public static void main(String[] args) {
-        System.out.println("Hello Cart!!");
+        System.out.println("Hello!!");
     }
 
 }
